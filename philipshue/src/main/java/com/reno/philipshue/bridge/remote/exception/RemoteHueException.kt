6309d62f5ec376b1a3162ed92cb4629663e35eba
reno-kim-km <reno.kim@kinemaster.com>
2020-01-06 19:32:11 +0900

    add RemoteHueException

---
+++ philipshue/src/main/java/com/reno/philipshue/bridge/remote/exception/RemoteHueException.kt
@@ -0,0 +1,9 @@
+package com.reno.philipshue.bridge.remote.exception
+
+/**
+ * reference
+ *
+ * https://developers.meethue.com/develop/hue-api/remote-hue-api-error-messages/
+ * */
+class RemoteHueException: Exception() {
+}
