e38e8bfef2b49d738b93238942a79e55651407d2
reno-kim-km <reno.kim@kinemaster.com>
2020-01-17 18:50:26 +0900

    save key at local
    the key is used to identify user

--- philipshue/src/main/java/com/reno/philipshue/bridge/remote/repository/token/HueWebViewClient.kt
+++ philipshue/src/main/java/com/reno/philipshue/bridge/remote/repository/token/HueWebViewClient.kt
@@ -1,18 +1,27 @@
 package com.reno.philipshue.bridge.remote.repository.token
 
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Build
+import android.webkit.JavascriptInterface
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.annotation.RequiresApi
+import com.google.gson.Gson
 import com.reno.philipshue.Log
 import com.reno.philipshue.bridge.remote.CALLBACK_URL
+import com.reno.philipshue.model.CallbackResult
 import com.reno.philipshue.model.Token
 import org.koin.java.KoinJavaComponent.inject
 
-class HueWebViewClient(private val doAfterCallback: () -> Unit) : WebViewClient() {
+class HueWebViewClient(private val doAfterCallback: () -> Unit) : WebViewClient(), IHueCallback {
     private val tokenRepository: ITokenRepository by inject(ITokenRepository::class.java)
+
+    override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+        super.onPageStarted(view, url, favicon)
+        view?.addJavascriptInterface(this, "Android")
+    }
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun shouldOverrideUrlLoading(
@@ -23,16 +32,7 @@ class HueWebViewClient(private val doAfterCallback: () -> Unit) : WebViewClient(
             "${request.url}\n${request.isForMainFrame}\n${request.method}\n${request.requestHeaders}".Log()
 
         if (request != null && isValidCallbackUrl(request.url)) {
-            val code = request.url.getQueryParameter("code")
-            val state = request.url.getQueryParameter("state")
-            val key = request.url.getQueryParameter("key")
-
-            if (code != null && state != null && key != null) {
-                tokenRepository.saveToken(Token(code, state))
-                tokenRepository.saveKey(key)
-            }
-
-            doAfterCallback.invoke()
+            view?.loadUrl("javascript:getCallbackResult()")
         }
 
         return false
@@ -46,5 +46,27 @@ class HueWebViewClient(private val doAfterCallback: () -> Unit) : WebViewClient(
     private fun isValidCallbackUrl(url: Uri): Boolean {
         return url.toString().contains(CALLBACK_URL)
     }
+
+    @JavascriptInterface
+    override fun getCallbackResult(result: String) {
+        result.Log()
+
+        val gson = Gson()
+        val callbackResult = gson.fromJson(result, CallbackResult::class.java)
+
+        val code = callbackResult.code
+        val state = callbackResult.state
+        val key = callbackResult.key
+
+        tokenRepository.saveToken(Token(code, state))
+        tokenRepository.saveKey(key)
+
+        doAfterCallback.invoke()
+    }
+
+}
+
+interface IHueCallback {
+    fun getCallbackResult(result: String)
 }
 
---
+++ philipshue/src/main/java/com/reno/philipshue/model/CallbackResult.kt
@@ -0,0 +1,7 @@
+package com.reno.philipshue.model
+
+data class CallbackResult(
+  val code:String,
+  val state:String,
+  val key:String
+)
